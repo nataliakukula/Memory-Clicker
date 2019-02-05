5665b13509b485334d168693dd03b82e1a6925b5
Kinky Cookie <kuki_kukula@yahoo.com>
2019-02-04 22:30:20 -0600

    Updates

---
+++ precache-manifest.81639a6bed03b5c413b3eb118f1b0d0c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d9752c7d5df2933515209cc6a760aa0",
+    "url": "/memory-clicker/static/media/gif-6.1d9752c7.gif"
+  },
+  {
+    "revision": "19e5beb241cd3b976bda",
+    "url": "/memory-clicker/static/css/main.424fcae0.chunk.css"
+  },
+  {
+    "revision": "c206b149c0351193f91e",
+    "url": "/memory-clicker/static/js/1.c206b149.chunk.js"
+  },
+  {
+    "revision": "7e63b4b384cb6a560774",
+    "url": "/memory-clicker/static/js/runtime~main.7e63b4b3.js"
+  },
+  {
+    "revision": "5ef0b70eb008bbc331b412da5ec2f0b0",
+    "url": "/memory-clicker/static/media/gif-1.5ef0b70e.gif"
+  },
+  {
+    "revision": "ed0ebb2d10dd91459dbb35144444331f",
+    "url": "/memory-clicker/static/media/gif-2.ed0ebb2d.gif"
+  },
+  {
+    "revision": "e3240235c7a58d4435dc4e28b443ea69",
+    "url": "/memory-clicker/static/media/gif-3.e3240235.gif"
+  },
+  {
+    "revision": "acc9eca3ea74246897578526011bc702",
+    "url": "/memory-clicker/static/media/gif-4.acc9eca3.gif"
+  },
+  {
+    "revision": "2d64d1c18e5145852962400ea56ec736",
+    "url": "/memory-clicker/static/media/gif-5.2d64d1c1.gif"
+  },
+  {
+    "revision": "19e5beb241cd3b976bda",
+    "url": "/memory-clicker/static/js/main.19e5beb2.chunk.js"
+  },
+  {
+    "revision": "a8db4ab84aaa3f788aac0c4379d4eb0a",
+    "url": "/memory-clicker/static/media/gif-7.a8db4ab8.gif"
+  },
+  {
+    "revision": "b99b70a0173cb23e6242e369cecf79ca",
+    "url": "/memory-clicker/static/media/gif-8.b99b70a0.gif"
+  },
+  {
+    "revision": "a0275314a16edcfdeb3b67ee07bb3008",
+    "url": "/memory-clicker/static/media/gif-9.a0275314.gif"
+  },
+  {
+    "revision": "f3c33b64cfd9708f49a9b1d1db830a22",
+    "url": "/memory-clicker/static/media/gif-10.f3c33b64.gif"
+  },
+  {
+    "revision": "657abfbbb1b0be18a2fdbba2a802631c",
+    "url": "/memory-clicker/static/media/gif-11.657abfbb.gif"
+  },
+  {
+    "revision": "9d9f2a7db2425edb7c3b9eccaa2a0b5f",
+    "url": "/memory-clicker/static/media/gif-12.9d9f2a7d.gif"
+  },
+  {
+    "revision": "395e5325b9ddcbdd28c3915bdf64b713",
+    "url": "/memory-clicker/static/media/background.395e5325.jpg"
+  },
+  {
+    "revision": "70a403dddd44af7c6467cade6007fc0a",
+    "url": "/memory-clicker/index.html"
+  }
+];
